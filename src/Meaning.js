085db08a0e31b17fff6b7a6e09ed58ee8e5d0104
NadiiaKotolup <nkotolup@gmail.com>
2022-10-12 15:36:52 +0200

    added phonetic

--- src/Meaning.js
+++ src/Meaning.js
@@ -10,11 +10,12 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              <strong>Definition:</strong>{definition.definition}
+              <strong>Definition:</strong>
+              {definition.definition}
               <br />
               <strong>Example:</strong>
               <em>{definition.example}</em>
-              <Synonyms senonyms={definition.synonyms} />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
